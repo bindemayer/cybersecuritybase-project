b28413dd7b64371acaab19d0c0d4640bade62fe5
Timo Mannermaa <timo.mannermaa@gmail.com>
2017-12-29 13:01:31 +0200

    Some functionalities and first vulnerabilities added

--- src/main/java/sec/project/config/SecurityConfiguration.java
+++ src/main/java/sec/project/config/SecurityConfiguration.java
@@ -21,8 +21,18 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         // no real security at the moment
+       // http.authorizeRequests()
+       //         .anyRequest().permitAll();
+       // http.formLogin()
+       //         .permitAll();
+       http.csrf().disable();
+       http.headers().frameOptions().sameOrigin();
+
         http.authorizeRequests()
-                .anyRequest().permitAll();
+                .antMatchers("/manage").authenticated().and().formLogin().permitAll();
+               // .anyRequest().authenticated().and().formLogin().permitAll();
+        
+        
     }
 
     @Autowired
--- src/main/java/sec/project/controller/SignupController.java
+++ src/main/java/sec/project/controller/SignupController.java
@@ -5,8 +5,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import sec.project.domain.Signup;
 import sec.project.repository.SignupRepository;
+//import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.ui.Model;
 
 @Controller
 public class SignupController {
@@ -28,6 +31,36 @@ public class SignupController {
     public String submitForm(@RequestParam String name, @RequestParam String address) {
         signupRepository.save(new Signup(name, address));
         return "done";
+    }
+    
+    @RequestMapping(value = "/remove/{name}", method = RequestMethod.POST)
+    public String remove(@PathVariable String name) {
+        if(name != null)
+        {
+           Signup remove = signupRepository.findByName(name);
+           signupRepository.delete(remove);
+           
+        }
+        return "redirect:/manage";
+    }
+    
+    @RequestMapping(value = "/manage", method = RequestMethod.GET)
+    public String manageForm(Model model) {
+        model.addAttribute("signups", signupRepository.findAll());
+        return "signuplist";
+    }
+    
+    @RequestMapping(value = "/delete/{name}", method = RequestMethod.GET)
+    public String deleteConfirmation(Model model, @PathVariable String name) {
+        if (name != null)
+        {
+            model.addAttribute("signup", signupRepository.findByName(name));
+            return "deleteConfirm";
+        }
+        else
+        {
+            return"redirect:/form";
+        }
     }
 
 }
